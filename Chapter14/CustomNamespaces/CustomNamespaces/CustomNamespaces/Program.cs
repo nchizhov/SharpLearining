77936d81586f31c8b68857990f46d60985e83531
nikolay <nikolay@inok.ru>
2020-11-15 11:21:29 +0200

    Finished Chapter 14

---
+++ Chapter14/CustomNamespaces/CustomNamespaces/CustomNamespaces/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyShapes;
+using Chapter14.My3DShapes;
+using The3DHexagon = Chapter14.My3DShapes.Hexagon;
+using bfHome = System.Runtime.Serialization.Formatters.Binary;
+
+namespace CustomNamespaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            The3DHexagon h2 = new The3DHexagon();
+            bfHome.BinaryFormatter b = new bfHome.BinaryFormatter();
+        }
+    }
+}
---
+++ Chapter14/VisualBasicCarClient/VisualBasicCarClient/VisualBasicCarClient/Module1.vb
@@ -0,0 +1,17 @@
+﻿Imports CarLibrary
+Module Module1
+
+    Sub Main()
+        Console.WriteLine("***** VB CarLibrary Client App *****")
+        Dim myMiniVan As New MiniVan()
+        myMiniVan.TurboBoost()
+        Dim mySportsCar As New SportsCar()
+        mySportsCar.TurboBoost()
+
+        Dim dreamCar As New PerformanceCar()
+        dreamCar.PetName = "Hank"
+        dreamCar.TurboBoost()
+        Console.ReadLine()
+    End Sub
+
+End Module
